8fd0e86276b458b5b65bb4def76a3bfd09a24d45
Hemant Koti <13030557+HemantKoti@users.noreply.github.com>
2021-04-28 17:57:15 -0400

    added command pattern

---
+++ CommandPattern/Commands/AddToCartCommand.cs
@@ -0,0 +1,50 @@
+﻿using CommandPattern.Models;
+using CommandPattern.Repository;
+using System;
+
+namespace CommandPattern.Commands
+{
+    public class AddToCartCommand : ICommand
+    {
+        private readonly IShoppingCartRepository shoppingCartRepository;
+        private readonly IProductRepository productRepository;
+        private readonly Product product;
+
+        public AddToCartCommand(IShoppingCartRepository shoppingCartRepository, IProductRepository productRepository, Product product)
+        {
+            this.shoppingCartRepository = shoppingCartRepository;
+            this.productRepository = productRepository;
+            this.product = product;
+        }
+
+        public bool CanExecute()
+        {
+            if (this.product == null)
+                return false;
+
+            return this.productRepository.GetStockFor(this.product.ArticleId) > 0;
+        }
+
+        public void Execute()
+        {
+            if (this.product == null)
+                return;
+
+            this.productRepository.DecreaseStockBy(this.product.ArticleId, 1);
+
+            this.shoppingCartRepository.Add(this.product);
+        }
+
+        public void Undo()
+        {
+            if (this.product == null)
+                return;
+
+            var lineItem = this.shoppingCartRepository.Get(this.product.ArticleId);
+
+            this.productRepository.IncreaseStockBy(this.product.ArticleId, lineItem.Quantity);
+
+            this.shoppingCartRepository.RemoveAll(this.product.ArticleId);
+        }
+    }
+}
---
+++ CommandPattern/Commands/ChangeQuantityCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using CommandPattern.Models;
+using CommandPattern.Repository;
+
+namespace CommandPattern.Commands
+{
+    public class ChangeQuantityCommand : ICommand
+    {
+        private readonly Operation operation;
+        private readonly IShoppingCartRepository shoppingCartRepository;
+        private readonly IProductRepository productRepository;
+        private readonly Product product;
+
+
+        public enum Operation
+        {
+            Increase,
+            Decrease
+        }
+
+        public ChangeQuantityCommand(Operation operation, IShoppingCartRepository shoppingCartRepository, IProductRepository productRepository, Product product)
+        {
+            this.operation = operation;
+            this.shoppingCartRepository = shoppingCartRepository;
+            this.productRepository = productRepository;
+            this.product = product;
+        }
+
+        public bool CanExecute()
+        {
+            return this.operation switch
+            {
+                Operation.Increase => this.productRepository.GetStockFor(this.product.ArticleId) - 1 >= 0,
+                Operation.Decrease => this.shoppingCartRepository.Get(this.product.ArticleId).Quantity != 0,
+                _ => false,
+            };
+        }
+
+        public void Execute()
+        {
+            switch (this.operation)
+            {
+                case Operation.Increase:
+                    this.productRepository.IncreaseStockBy(this.product.ArticleId, 1);
+                    this.shoppingCartRepository.DecreaseQuantity(this.product.ArticleId);
+                    break;
+                case Operation.Decrease:
+                    this.productRepository.DecreaseStockBy(this.product.ArticleId, 1);
+                    this.shoppingCartRepository.IncreaseQuantity(this.product.ArticleId);
+                    break;
+            }
+        }
+
+        public void Undo()
+        {
+            switch (operation)
+            {
+                case Operation.Decrease:
+                    productRepository.DecreaseStockBy(product.ArticleId, 1);
+                    shoppingCartRepository.IncreaseQuantity(product.ArticleId);
+                    break;
+                case Operation.Increase:
+                    productRepository.IncreaseStockBy(product.ArticleId, 1);
+                    shoppingCartRepository.DecreaseQuantity(product.ArticleId);
+                    break;
+            }
+        }
+    }
+}
