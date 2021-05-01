3dcffc0994e8db828b407eceaabe8205a33acf1b
Hemant Koti <13030557+HemantKoti@users.noreply.github.com>
2021-04-30 23:17:01 -0400

    added null object pattern

---
+++ NullObject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NullObject.Entities;
+using NullObject.Services;
+using NullObject.View;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            
+                throw new ArgumentNullException(nameof(args));            
+
+            LearnerService learnerService = new();
+            ILearner learner = learnerService.GetCurrentLearner();
+
+            LearnerView view = new(learner);
+            view.RenderView();
+        }
+    }
+}
---
+++ SpecificationPatttern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpecificationPatttern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
